9378796f5a8c1d45c4721dc5a2b3dc72b2655d56
Zachary Hayward <zmhhayward@gmail.com>
2024-05-23 16:47:19 -0500

    Fix changed Quiz to sudo App component

--- client/index.tsx
+++ client/index.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-import App from './components/App.tsx'
 import Quiz from './components/Quiz.tsx'
 
 const queryClient = new QueryClient()
@@ -10,7 +9,6 @@ const queryClient = new QueryClient()
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
-      <App />
       <Quiz />
       <ReactQueryDevtools />
     </QueryClientProvider>
